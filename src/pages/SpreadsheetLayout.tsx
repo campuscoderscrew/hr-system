6dab547637160300061753488e266a64a224094c
Kevin Zhang <kevinzhang@Mac.wlan>
2026-07-06 00:09:16 -0400

    Messed too much with unreadable grid layouts

--- src/pages/SpreadsheetLayout.tsx
+++ src/pages/SpreadsheetLayout.tsx
@@ -1,10 +1,66 @@
 // TODO: Change all prop types to reflect types defined in
 // `~/src/modal/types.ts`
 
-const TeamLayout = (props: any) => {};
+import { cn } from "~/utils";
 
-const DepartmentLayout = (props: any) => {};
+const TeamLayout = (props: any) => {
+  const { team } = props;
 
-const OperationsSectorLayout = (props: any) => {};
+  const numCols = team.members.length; // Number of columns in the table
+  const tableCols = [
+    // TODO
+    // Object.keys(), // Team type 
+    ...team.members,
+  ];
+
+  return (
+    <div
+      className="flex flex-col divide-y
+        rounded-lg shadow-lg overflow-hidden 
+        [&_span]:px-4 [&_span]:py-2 [&_span]:-mx-[2px]"
+    >
+      {/* Table header */}
+      <span className="bg-rose-100">Test</span>
+
+      <div className="flex gap-[2px] divide-x">
+        {team.members.map((member: any, index: number) => (
+          <div className="flex flex-col divide-y">
+            <span
+              className={
+                member.role === "project lead" ||
+                member.role === "secondary lead"
+                  ? "bg-yellow-300"
+                  : "bg-blue-300"
+              }
+            >
+              {member.role}
+            </span>
+            <span>{member.name}</span>
+            <span>{member.discord}</span>
+            <span>{member.totalProficiency}</span>
+            <span>{member.availability}</span>
+            <span>{member.github}</span>
+            <span>{member.email}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const DepartmentLayout = (props: any) => {
+  const { department } = props;
+  return (
+    <div className="flex flex-col gap-8">
+      {department.teams.map((team: any) => (
+        <TeamLayout team={team} />
+      ))}
+    </div>
+  );
+};
+
+const OperationsSectorLayout = (props: any) => {
+  return <table></table>;
+};
 
 export { TeamLayout, DepartmentLayout, OperationsSectorLayout };
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,30 @@
-import React from "react"
+import React from "react";
 
-import NavBar from "~/components/navbar"
+import NavBar from "~/components/navbar";
+import { TeamLayout } from "./SpreadsheetLayout";
 
 export default function About() {
-    return (
-        <div className="">
-            <NavBar/>
-            About Boilerplate
-        </div>
-    )
+  const team = {
+    name: "Team Lemon",
+    members: new Array(4).fill({
+      role: "project lead",
+      name: "Jane Doe",
+      discord: "janeDoe",
+      totalProficiency: 9,
+      availability: 10,
+      github: "janeDoe",
+      email: "jane@doe.com",
+    }),
+    teamLead: {},
+  };
+
+  return (
+    <div className="">
+      <NavBar />
+      About Boilerplate
+      <div className="h-screen grid place-items-center ">
+        <TeamLayout team={team} />
+      </div>
+    </div>
+  );
 }
