2079cdeb30769d195973ac4e7c8bf89c16a77502
Kevin Zhang <kevinzhang@Mac.wlan>
2026-07-13 00:00:30 -0400

    Assuming corresponding role/supervisor lists to determine member role

--- src/pages/SpreadsheetLayout.tsx
+++ src/pages/SpreadsheetLayout.tsx
@@ -1,66 +1,96 @@
 // TODO: Change all prop types to reflect types defined in
 // `~/src/modal/types.ts`
 
+import type { ClassValue } from "clsx";
+import type {
+  Department,
+  Member,
+  OperationSector,
+  Role,
+  Team,
+} from "~/model/types";
 import { cn } from "~/utils";
 
-const TeamLayout = (props: any) => {
-  const { team } = props;
-
-  const numCols = team.members.length; // Number of columns in the table
-  const tableCols = [
-    // TODO
-    // Object.keys(), // Team type 
-    ...team.members,
-  ];
+const TeamLayout = (props: { className?: ClassValue[]; team: Team | any }) => {
+  const { className, team } = props;
 
   return (
     <div
-      className="flex flex-col divide-y
-        rounded-lg shadow-lg overflow-hidden 
-        [&_span]:px-4 [&_span]:py-2 [&_span]:-mx-[2px]"
+      className={cn(
+        "flex flex-col divide-y",
+        "bg-neutral-50 rounded-lg shadow-md overflow-hidden",
+        "[&_span]:px-4 [&_span]:py-2 [&_span]:-mx-[2px]",
+        className,
+      )}
     >
       {/* Table header */}
-      <span className="bg-rose-100">Test</span>
+      <span className="bg-rose-100">{team.name}</span>
 
       <div className="flex gap-[2px] divide-x">
-        {team.members.map((member: any, index: number) => (
-          <div className="flex flex-col divide-y">
-            <span
-              className={
-                member.role === "project lead" ||
-                member.role === "secondary lead"
-                  ? "bg-yellow-300"
-                  : "bg-blue-300"
-              }
-            >
-              {member.role}
-            </span>
-            <span>{member.name}</span>
-            <span>{member.discord}</span>
-            <span>{member.totalProficiency}</span>
-            <span>{member.availability}</span>
-            <span>{member.github}</span>
-            <span>{member.email}</span>
-          </div>
-        ))}
+        {team.members.map((member: Member, index: number) => {
+          // Assumes roles and supervisors are of same length with corresponding entries
+          const supervisorIdx = member.supervisors.indexOf(team.teamLead);
+          const currentRole = member.currentRoles[supervisorIdx].role;
+
+          return (
+            <div className="flex flex-col divide-y">
+              <span>{currentRole}</span>
+              <span>{member.name}</span>
+              <span>{member.discord}</span>
+              {member.proficiency && <span>{member.proficiency}</span>}
+              <span>{member.availability}</span>
+              {member.github && <span>{member.github}</span>}
+              <span>{member.email}</span>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 };
 
-const DepartmentLayout = (props: any) => {
-  const { department } = props;
+const DepartmentLayout = (props: {
+  className?: ClassValue[];
+  department: Department | any;
+}) => {
+  const { className, department } = props;
   return (
-    <div className="flex flex-col gap-8">
-      {department.teams.map((team: any) => (
-        <TeamLayout team={team} />
+    <div
+      className={cn(
+        "p-4 flex flex-col gap-2 bg-yellow-300 rounded-xl",
+        className,
+      )}
+    >
+      <span className="text-lg font-semibold">{department.name}</span>
+
+      {department.teams.map((team: Team, index: number) => (
+        <TeamLayout key={`team-${index}-${team.name}`} team={team} />
       ))}
     </div>
   );
 };
 
-const OperationsSectorLayout = (props: any) => {
-  return <table></table>;
+const OperationsSectorLayout = (props: {
+  className?: ClassValue[];
+  sector: OperationSector | any;
+}) => {
+  const { className, sector } = props;
+  return (
+    <div
+      className={cn("p-4 flex flex-col gap-2 bg-black rounded-xl", className)}
+    >
+      <span className="text-neutral-50 text-lg font-semibold">
+        {sector.name}
+      </span>
+
+      {sector.departments.map((department: Department, index: number) => (
+        <DepartmentLayout
+          key={`department-${index}-${department.name}`}
+          department={department}
+        />
+      ))}
+    </div>
+  );
 };
 
 export { TeamLayout, DepartmentLayout, OperationsSectorLayout };
