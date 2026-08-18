ede099898bde07ca26930f0181368551bf1e8c3b
Kevin Zhang <kevinzhang@Mac.wlan>
2026-08-17 20:08:43 -0400

    Adjusted model mock data to match email changes

--- utils/model/data.ts
+++ utils/model/data.ts
@@ -5,14 +5,14 @@ export const members: Membership[] = [
   {
     name: "John Doe",
     discord: "discord_username",
-    email: "john_doe@gmail.com",
+    emails: ["john_doe@gmail.com"],
     role: "Developer" as const,
     currentRoles: [{ role: "Developer" as const, startDate: new Date("1970-01-01"), supervisor: "Jane Smith" }],
   },
   {
     name: "Jane Smith",
     discord: "discord_username",
-    email: "jane_smith@gmail.com",
+    emails: ["jane_smith@gmail.com"],
     role: "Team Lead" as const,
     currentRoles: [{ role: "Team Lead" as const, startDate: new Date("1970-01-01"), supervisor: "John Doe" }],
   }
