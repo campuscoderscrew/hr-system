f68b801ee6e2fd668c41c18c44292e7104a134d5
Kevin Zhang <kevinzhang@Mac.wlan>
2026-08-24 18:10:33 -0400

    Finished testing Tarjan's Algorithm

--- utils/model/data.ts
+++ utils/model/data.ts
@@ -7,13 +7,13 @@ export const members: Membership[] = [
     discord: "discord_username",
     emails: ["john_doe@gmail.com"],
     role: "Developer" as const,
-    currentRoles: [{ role: "Developer" as const, startDate: new Date("1970-01-01"), supervisor: "Jane Smith" }],
+    currentRoles: [{ role: "Developer" as const, startDate: new Date("1970-01-01"), supervisor: "jane_smith@gmail.com" }],
   },
   {
     name: "Jane Smith",
     discord: "discord_username",
     emails: ["jane_smith@gmail.com"],
     role: "Team Lead" as const,
-    currentRoles: [{ role: "Team Lead" as const, startDate: new Date("1970-01-01"), supervisor: "John Doe" }],
+    currentRoles: [{ role: "Team Lead" as const, startDate: new Date("1970-01-01"), supervisor: "john_doe@gmail.com" }],
   }
 ];
